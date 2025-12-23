40d7debf2b573ed507afd123cd9e830f00a84c70
Xlkki <dmitryfilatov@inbox.ru>
2025-12-23 19:45:32 +0500

    Update #25: - Adding BCELUtils#getAnnotation methods - Adding BCELUtils#getOptionalFieldValue method - Adding BCELUtils#getFieldValue method - Adding BCELUtils#hasAnnotation methods - Adding BCELUtils#hasAnnotatedMethod methods - Adding BCELUtils#hasAnnotatedField methods - Adding BCELUtils#hasAnnotatedNonStaticMethod methods - Adding BCELUtils#hasAnnotatedStaticMethod methods - Adding BCELUtils#hasAnnotatedNonStaticField methods - Adding BCELUtils#hasAnnotatedStaticField methods

--- src/main/java/ru/xikki/libraries/reflections/bcel/BCELUtils.java
+++ src/main/java/ru/xikki/libraries/reflections/bcel/BCELUtils.java
@@ -3,10 +3,7 @@ package ru.xikki.libraries.reflections.bcel;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
-import org.apache.bcel.classfile.Field;
-import org.apache.bcel.classfile.JavaClass;
-import org.apache.bcel.classfile.Method;
-import org.apache.bcel.classfile.Utility;
+import org.apache.bcel.classfile.*;
 import org.apache.bcel.generic.*;
 import ru.xikki.libraries.reflections.ReflectionUtils;
 import ru.xikki.libraries.reflections.condition.FieldCondition;
@@ -15,6 +12,8 @@ import ru.xikki.libraries.reflections.condition.MethodCondition;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @UtilityClass
 public final class BCELUtils {
@@ -217,6 +216,454 @@ public final class BCELUtils {
 	@NonNull
 	public String getAnnotationSignature(@NonNull Class<? extends Annotation> annotationClass) {
 		return "L" + Utility.packageToPath(annotationClass.getName()) + ";";
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL class by annotation signature
+	 *
+	 * @param javaClass           BCEL class
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull JavaClass javaClass, @NonNull String annotationSignature) {
+		return Arrays.stream(javaClass.getAnnotationEntries())
+				.filter((entry) -> entry.getAnnotationType().equals(annotationSignature))
+				.findFirst()
+				.orElse(null);
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL class by BCEL type
+	 *
+	 * @param javaClass BCEL class
+	 * @param type      BCEL type
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull JavaClass javaClass, @NonNull ObjectType type) {
+		return BCELUtils.getAnnotation(javaClass, type.getSignature());
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL class by java annotation class
+	 *
+	 * @param javaClass       BCEL class
+	 * @param annotationClass Java annotation class
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull JavaClass javaClass, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.getAnnotation(javaClass, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL method by annotation signature
+	 *
+	 * @param method              BCEL method
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull Method method, @NonNull String annotationSignature) {
+		return Arrays.stream(method.getAnnotationEntries())
+				.filter((entry) -> entry.getAnnotationType().equals(annotationSignature))
+				.findFirst()
+				.orElse(null);
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL method by BCEL type
+	 *
+	 * @param method BCEL method
+	 * @param type   BCEL type
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull Method method, @NonNull ObjectType type) {
+		return BCELUtils.getAnnotation(method, type.getSignature());
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL method by java annotation class
+	 *
+	 * @param method          BCEL method
+	 * @param annotationClass Java annotation class
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull Method method, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.getAnnotation(method, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL field by annotation signature
+	 *
+	 * @param field               BCEL field
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull Field field, @NonNull String annotationSignature) {
+		return Arrays.stream(field.getAnnotationEntries())
+				.filter((entry) -> entry.getAnnotationType().equals(annotationSignature))
+				.findFirst()
+				.orElse(null);
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL field by BCEL type
+	 *
+	 * @param field BCEL field
+	 * @param type  BCEL type
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull Field field, @NonNull ObjectType type) {
+		return BCELUtils.getAnnotation(field, type.getSignature());
+	}
+
+	/**
+	 * Get BCEL annotation entry from BCEL field by java annotation class
+	 *
+	 * @param field           BCEL field
+	 * @param annotationClass Java annotation class
+	 * @return BCEL annotation entry
+	 */
+	public AnnotationEntry getAnnotation(@NonNull Field field, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.getAnnotation(field, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Get BCEL annotation entry field value by field name
+	 *
+	 * @param entry BCEL annotation entry
+	 * @param name  field name
+	 * @return optional with field value (in string format)
+	 */
+	public Optional<String> getOptionalFieldValue(@NonNull AnnotationEntry entry, @NonNull String name) {
+		return Arrays.stream(entry.getElementValuePairs())
+				.filter((pair) -> pair.getNameString().equals(name))
+				.findFirst()
+				.map(ElementValuePair::getValue)
+				.map(ElementValue::stringifyValue);
+	}
+
+	/**
+	 * Get BCEL annotation entry field value by field name
+	 *
+	 * @param entry BCEL annotation entry
+	 * @param name  field name
+	 * @return field value (in string)
+	 */
+	public String getFieldValue(@NonNull AnnotationEntry entry, @NonNull String name) {
+		return BCELUtils.getOptionalFieldValue(entry, name)
+				.orElse(null);
+	}
+
+	/**
+	 * Check if BCEL class has annotation by signature
+	 *
+	 * @param javaClass           BCEL class
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if class has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull JavaClass javaClass, @NonNull String annotationSignature) {
+		return Arrays.stream(javaClass.getAnnotationEntries())
+				.map(AnnotationEntry::getAnnotationType)
+				.anyMatch(Predicate.isEqual(annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL class has annotation by BCEL type
+	 *
+	 * @param javaClass BCEL class
+	 * @param type      BCEL type
+	 * @return true if class has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull JavaClass javaClass, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotation(javaClass, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL class has annotation by java annotation class
+	 *
+	 * @param javaClass       BCEL class
+	 * @param annotationClass Java annotation class
+	 * @return true if class has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull JavaClass javaClass, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotation(javaClass, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Check if BCEL method has annotation by annotation signature
+	 *
+	 * @param method              BCEL method
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if method has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull Method method, @NonNull String annotationSignature) {
+		return Arrays.stream(method.getAnnotationEntries())
+				.map(AnnotationEntry::getAnnotationType)
+				.anyMatch(Predicate.isEqual(annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL method has annotation by BCEL type
+	 *
+	 * @param method BCEL method
+	 * @param type   BCEL type
+	 * @return true if method has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull Method method, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotation(method, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL method has annotation by java annotation class
+	 *
+	 * @param method          BCEL method
+	 * @param annotationClass Java annotation class
+	 * @return true if method has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull Method method, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotation(method, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Check if BCEL field has annotation by annotation signature
+	 *
+	 * @param field               BCEL field
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if field has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull org.apache.bcel.classfile.Field field, @NonNull String annotationSignature) {
+		return Arrays.stream(field.getAnnotationEntries())
+				.map(AnnotationEntry::getAnnotationType)
+				.anyMatch(Predicate.isEqual(annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL field has annotation by BCEL type
+	 *
+	 * @param field BCEL field
+	 * @param type  BCEL type
+	 * @return true if field has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull org.apache.bcel.classfile.Field field, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotation(field, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL field has annotation by java annotation class
+	 *
+	 * @param field           BCEL field
+	 * @param annotationClass Java annotation class
+	 * @return true if method has annotation, otherwise false
+	 */
+	public boolean hasAnnotation(@NonNull org.apache.bcel.classfile.Field field, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotation(field, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Check if BCEL class has method with specified annotation by annotation signature
+	 *
+	 * @param javaClass           BCEL class
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if class has method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedMethod(@NonNull JavaClass javaClass, @NonNull String annotationSignature) {
+		return Arrays.stream(javaClass.getMethods())
+				.anyMatch((method) -> BCELUtils.hasAnnotation(method, annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL class has method with specified annotation by BCEL type
+	 *
+	 * @param javaClass BCEL class
+	 * @param type      BCEL type
+	 * @return true if class has method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedMethod(@NonNull JavaClass javaClass, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotatedMethod(javaClass, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL class has method with specified annotation by java annotation class
+	 *
+	 * @param javaClass       BCEL class
+	 * @param annotationClass Java annotation class
+	 * @return true if class has method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedMethod(@NonNull JavaClass javaClass, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotatedMethod(javaClass, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Check if BCEL class has method with specified annotation by annotation signature
+	 *
+	 * @param javaClass           BCEL class
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if class has method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedStaticMethod(@NonNull JavaClass javaClass, @NonNull String annotationSignature) {
+		return Arrays.stream(javaClass.getMethods())
+				.filter(Method::isStatic)
+				.anyMatch((method) -> BCELUtils.hasAnnotation(method, annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL class has method with specified annotation by BCEL type
+	 *
+	 * @param javaClass BCEL class
+	 * @param type      BCEL type
+	 * @return true if class has method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedStaticMethod(@NonNull JavaClass javaClass, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotatedStaticMethod(javaClass, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL class has method with specified annotation by java annotation class
+	 *
+	 * @param javaClass       BCEL class
+	 * @param annotationClass Java annotation class
+	 * @return true if class has method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedStaticMethod(@NonNull JavaClass javaClass, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotatedStaticMethod(javaClass, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Check if BCEL class has non-method with specified annotation by annotation signature
+	 *
+	 * @param javaClass           BCEL class
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if class has non-method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedNonStaticMethod(@NonNull JavaClass javaClass, @NonNull String annotationSignature) {
+		return Arrays.stream(javaClass.getMethods())
+				.filter(Predicate.not(Method::isStatic))
+				.anyMatch((method) -> BCELUtils.hasAnnotation(method, annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL class has non-method with specified annotation by BCEL type
+	 *
+	 * @param javaClass BCEL class
+	 * @param type      BCEL type
+	 * @return true if class has non-method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedNonStaticMethod(@NonNull JavaClass javaClass, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotatedNonStaticMethod(javaClass, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL class has non-method with specified annotation by java annotation class
+	 *
+	 * @param javaClass       BCEL class
+	 * @param annotationClass Java annotation class
+	 * @return true if class has non-method with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedNonStaticMethod(@NonNull JavaClass javaClass, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotatedNonStaticMethod(javaClass, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Check if BCEL class has field with specified annotation by annotation signature
+	 *
+	 * @param javaClass           BCEL class
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if class has field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedField(@NonNull JavaClass javaClass, @NonNull String annotationSignature) {
+		return Arrays.stream(javaClass.getFields())
+				.anyMatch((field) -> BCELUtils.hasAnnotation(field, annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL class has field with specified annotation by BCEL type
+	 *
+	 * @param javaClass BCEL class
+	 * @param type      BCEL type
+	 * @return true if class has field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedField(@NonNull JavaClass javaClass, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotatedField(javaClass, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL class has field with specified annotation by java annotation class
+	 *
+	 * @param javaClass       BCEL class
+	 * @param annotationClass Java annotation class
+	 * @return true if class has field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedField(@NonNull JavaClass javaClass, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotatedField(javaClass, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Check if BCEL class has field with specified annotation by annotation signature
+	 *
+	 * @param javaClass           BCEL class
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if class has field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedStaticField(@NonNull JavaClass javaClass, @NonNull String annotationSignature) {
+		return Arrays.stream(javaClass.getFields())
+				.filter(org.apache.bcel.classfile.Field::isStatic)
+				.anyMatch((field) -> BCELUtils.hasAnnotation(field, annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL class has field with specified annotation by BCEL type
+	 *
+	 * @param javaClass BCEL class
+	 * @param type      BCEL type
+	 * @return true if class has field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedStaticField(@NonNull JavaClass javaClass, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotatedStaticField(javaClass, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL class has field with specified annotation by java annotation class
+	 *
+	 * @param javaClass       BCEL class
+	 * @param annotationClass Java annotation class
+	 * @return true if class has field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedStaticField(@NonNull JavaClass javaClass, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotatedStaticField(javaClass, BCELUtils.getAnnotationSignature(annotationClass));
+	}
+
+	/**
+	 * Check if BCEL class has non-field with specified annotation by annotation signature
+	 *
+	 * @param javaClass           BCEL class
+	 * @param annotationSignature Annotation signature. For example <code>Ljava.lang.FunctionalInterface;</code>
+	 * @return true if class has non-field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedNonStaticField(@NonNull JavaClass javaClass, @NonNull String annotationSignature) {
+		return Arrays.stream(javaClass.getFields())
+				.filter(Predicate.not(org.apache.bcel.classfile.Field::isStatic))
+				.anyMatch((field) -> BCELUtils.hasAnnotation(field, annotationSignature));
+	}
+
+	/**
+	 * Check if BCEL class has non-field with specified annotation by BCEL type
+	 *
+	 * @param javaClass BCEL class
+	 * @param type      BCEL type
+	 * @return true if class has non-field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedNonStaticField(@NonNull JavaClass javaClass, @NonNull ObjectType type) {
+		return BCELUtils.hasAnnotatedNonStaticField(javaClass, type.getSignature());
+	}
+
+	/**
+	 * Check if BCEL class has non-field with specified annotation by java annotation class
+	 *
+	 * @param javaClass       BCEL class
+	 * @param annotationClass Java annotation class
+	 * @return true if class has non-field with specified annotation, otherwise false
+	 */
+	public boolean hasAnnotatedNonStaticField(@NonNull JavaClass javaClass, @NonNull Class<? extends Annotation> annotationClass) {
+		return BCELUtils.hasAnnotatedNonStaticField(javaClass, BCELUtils.getAnnotationSignature(annotationClass));
 	}
 
 
